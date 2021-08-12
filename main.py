653835747938f3d6c623e821ab6c9a13964bb026
TalhaUsuf <47290815+TalhaUsuf@users.noreply.github.com>
2021-08-13 02:15:29 +0500

    fixed:error in the code got fixed, works on widerface dataset format

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import random
-
+from rich.console import Console
 import cv2
 import os
 import glob
 import numpy as np
+from tqdm import tqdm
 from PIL import Image
+from pathlib import Path
 
 OUTPUT_SIZE = (600, 600)  # Height, Width
 SCALE_RANGE = (0.3, 0.7)
 FILTER_TINY_SCALE = 1 / 50  # if height or width lower than this scale, drop it.
 
-ANNO_DIR = 'dataset/WiderPerson/Annotations/'
-IMG_DIR = 'dataset/WiderPerson/Images/'
+ANNO_DIR = './dataset/WiderPerson/Annotations/'
+IMG_DIR = './dataset/WiderPerson/Images/'
 
 category_name = ['background', 'person']
 
@@ -19,23 +21,28 @@ category_name = ['background', 'person']
 def main():
     img_paths, annos = get_dataset(ANNO_DIR, IMG_DIR)
 
-    idxs = random.sample(range(len(annos)), 4)
+    for idx in tqdm(range(100), desc="AUGMENTATION-NO:", total=100):
 
-    new_image, new_annos = update_image_and_anno(img_paths, annos,
-                                                 idxs,
-                                                 OUTPUT_SIZE, SCALE_RANGE,
-                                                 filter_scale=FILTER_TINY_SCALE)
+        # sample randomly indices out of a list of 22 annotations
+        idxs = random.sample(range(len(annos)), 4)
 
-    cv2.imwrite('img/output.jpg', new_image)
-    for anno in new_annos:
-        start_point = (int(anno[1] * OUTPUT_SIZE[1]), int(anno[2] * OUTPUT_SIZE[0]))
-        end_point = (int(anno[3] * OUTPUT_SIZE[1]), int(anno[4] * OUTPUT_SIZE[0]))
-        cv2.rectangle(new_image, start_point, end_point, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.imwrite('img/output_box.jpg', new_image)
+        new_image, new_annos = update_image_and_anno(img_paths, annos,
+                                                     idxs,
+                                                     OUTPUT_SIZE, SCALE_RANGE,
+                                                     filter_scale=FILTER_TINY_SCALE)
 
-    new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
-    new_image = Image.fromarray(new_image.astype(np.uint8))
-    new_image.show()
+        cv2.imwrite(f'./img/{idx}_without_boxes.jpg', new_image)
+        for anno in new_annos:
+            start_point = (int(anno[1] * OUTPUT_SIZE[1]), int(anno[2] * OUTPUT_SIZE[0]))
+            end_point = (int(anno[3] * OUTPUT_SIZE[1]), int(anno[4] * OUTPUT_SIZE[0]))
+            cv2.rectangle(new_image, start_point, end_point, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.imwrite(f'img/{idx}_with_boxes.jpg', new_image)
+        # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        #                     disable showing images
+        # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        # new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+        # new_image = Image.fromarray(new_image.astype(np.uint8))
+        # new_image.show()
 
 
 def update_image_and_anno(all_img_list, all_annos, idxs, output_size, scale_range, filter_scale=0.):
@@ -101,13 +108,25 @@ def get_dataset(anno_dir, img_dir):
 
     img_paths = []
     annos = []
-    for anno_file in glob.glob(os.path.join(anno_dir, '*.txt')):
+    base_path = Path(".")
+    images = base_path / "dataset" / "WiderPerson" / "Images"
+    annotations = base_path / "dataset" / "WiderPerson" / "Annotations"
+    # Console().print(f"image path==>{images}")
+    # Console().print(f"annotations path==>{annotations}")
+    # Console().rule(title="paths", align="center", style="cyan")
+    annot_paths = list(annotations.glob("*.txt"))
+    # for anno_file in glob.glob(os.path.join(anno_dir, '*.txt')):
+    for anno_file in annot_paths:
+        anno_file = anno_file.as_posix()
         anno_id = anno_file.split('/')[-1].split('.')[0]
 
         with open(anno_file, 'r') as f:
             num_of_objs = int(f.readline())
 
-            img_path = os.path.join(img_dir, f'{anno_id}.jpg')
+            # img_path = os.path.join(img_dir, f'{anno_id}.jpg')
+            # Console().print(f"current dir. ===> {Path().cwd()}")
+            img_path = Path("./"+img_dir,f'{anno_id}.jpg').as_posix()
+            # Console().print(f"image path is : {img_path}")
             img = cv2.imread(img_path)
             img_height, img_width, _ = img.shape
             del img
@@ -129,8 +148,11 @@ def get_dataset(anno_dir, img_dir):
             if not boxes:
                 continue
 
-        img_paths.append(img_path)
+        img_paths.append(img_path) # img_path is a list of images
         annos.append(boxes)
+    with Console().status("lists made ....", spinner="bouncingBall"):
+        Console().print(f"{img_paths}")
+        # Console().print(f"{annos}")
     return img_paths, annos
 
 
